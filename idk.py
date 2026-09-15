f1f5a1e7306cb0d1ef1cbfea953fc88bc711c0eb
OmegaRty <ricardcavrav@gmail.com>
2026-09-15 10:02:04 +0200

    test

---
+++ idk.py
@@ -0,0 +1,10 @@
+
+def Average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+
+print(f"The average of the three numbers is: {Average(num1, num2, num3)}")
